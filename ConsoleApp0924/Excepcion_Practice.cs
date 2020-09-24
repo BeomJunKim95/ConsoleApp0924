c33ef57b1a9789c79ac1c6c2e77483dec2074e15
김범준 <rktqjawns@naver.com>
2020-09-24 18:25:31 +0900

    초기 커밋.

---
+++ ConsoleApp0924/Excepcion_Practice.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0924
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region 1. 예외처리구문 정의
+			//try // 예외 처리 구문 : try ~ catch
+			//    // try안에 선언되면 try안에서 밖에 못씀
+			//{
+			//	int p = 0;
+			//	int k = 10 / p;  //0으로 나눌수 없어 예외 발생
+			//	Console.WriteLine(k);
+			//}
+			//catch //예외가 발생했을때만 사용 아니면 다음으로 넘어간다
+			//{
+			//	Console.WriteLine("0으로 나눌수 없습니다");
+			//}
+			//string eee = Console.ReadLine(); // 예외가 발생해도 try~catch구문을 사용하면 끝나지않고 다음 코드로 넘어온다
+			#endregion
+
+			#region 2. 예외처리 종류 구분하는법
+			//try // 예외 처리 구문 : try ~ catch
+			//{
+			//	int[] arr = new int[5] { 1, 2, 0, 4, 5 };
+			//	Console.Write("몇번째 배열의 값으로 계산할까요? : ");
+			//	int idx = int.Parse(Console.ReadLine());
+
+			//	int p = arr[idx];
+			//	int k = 10 / p;  
+			//	Console.WriteLine(k);
+			//}
+			//catch (DivideByZeroException) //예외의 종류에 따라 구분을 하고싶으면 catch절을 사용 (if ~ else if 와 비슷)
+			//{
+			//	Console.WriteLine("0으로 나눌수 없습니다");
+			//}
+			//catch (IndexOutOfRangeException)
+			//{
+			//	Console.WriteLine("0~4 범위안의 수를 입력하세요.");
+			//}
+			////catch (Exception) //try~catch구문의 보험처럼 쓰인다 모든 에러는 Exception의 자식이니 이게 다 잡아줄거임
+			////{
+			////	Console.WriteLine("처리중 오류가 발생했습니다. 다시한번 시도해주세요.");
+			////}
+			//catch (Exception err) //err변수를 생성후 ==> 이것이 else문과 비슷
+			//{
+			//	Console.WriteLine(err.Message);// 예외를 출력해주는 Message를 사용해 뭐 때문에 오류가 난지 보여준다
+			//}
+			//// 이것만 써도 대부분 오류처리가 다된다 위에 catch절을 사용하는 이유는 최대한 사용자 친화적인 멘트를 보여주기위해
+
+			//string eee = Console.ReadLine(); // 예외가 발생해도 try~catch구문을 사용하면 끝나지않고 다음 코드로 넘어온다
+			#endregion
+
+			#region 예외 발생 : Throw
+			// 각각의 메서드에서 예외처리 하지말고 throw로 던져서 공통된 한자리에서만 예외처리를 한다면 더 좋기때문에 쓰는것이다
+			// 결론은 다 던져서 한곳에서 공통된 방식으로 예외처리를 하면 좋다 ex) Main 함수
+			try
+			{
+				BBB(); //메서드로 생성
+				string eee = Console.ReadLine(); // 예외가 발생해도 try~catch구문을 사용하면 끝나지않고 다음 코드로 넘어온다
+			}
+			catch (Exception err)
+			{
+				Console.WriteLine(err.Message);
+			}
+			Console.ReadLine();
+			#endregion
+		}
+		private static void BBB()
+		{
+			AAA();
+		}
+		static void AAA()
+		{
+			//try // 예외 처리 구문 : try ~ catch
+			//{
+				int[] arr = new int[5] { 1, 2, 0, 4, 5 };
+				Console.Write("몇번째 배열의 값으로 계산할까요? : ");
+				int idx = int.Parse(Console.ReadLine());
+
+				if (idx < 0 || idx > 4)
+				{
+					//만약 throw를 했을때 아래 캐치문이 없다면 자신을 호출한 쪽으로 보내 호출한 쪽의 캐치문을 사용
+					throw new /*Application*/Exception("0~4의 수 입력하세요"); // 한줄처리 (비지니스 로직 처리할때 쓴다)
+					
+					//Console.WriteLine("0~4의 수 입력하세요");
+					//return;
+				}
+
+				int p = arr[idx];
+				int k = 10 / p;
+				Console.WriteLine(k);
+			//}
+			//catch (Exception err)
+			//{
+			//	throw err;
+			//}
+		}
+
+	}
+	
+}
+
---
+++ ConsoleApp0924/SingleTone1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0924
+{
+	class SingleTone1
+	{
+		static SingleTone1 instance; //이게 static이 아니면 staic메서드인 CreateInstance()가 호출을 못함
+		private SingleTone1() // 기본 생성자를 프라이빗으로 해서 바깥에서 new를 못함 (인스턴스 생성)
+							  // 인스턴스를 하나만 만들어서 공통으로 쓰게 하는게 싱글톤
+		{
+		}
+
+		public static SingleTone1 CreateInstance() // 생성자를 만들어 주는거기 떄문에 반환타입은 생성자의 타입인 클래스명
+		{
+			//SingleTone1 instance; // 이게 전역으로 가야 메서드가 끝날때 사라지지 않는다
+			if (instance == null)
+				instance = new SingleTone1(); // 클래스 내부이기 때문에 private이여도 new할수있음
+
+			return instance; // 이미 인스턴스가 생성 되어 있으면 if문을 건너 뛰고 이미 생성된 인스턴스를 반환
+		}
+		public void Method()
+		{
+			Console.WriteLine("메서드 실행");
+		}
+	}
+
+	class Test
+	{
+		static void Main()
+		{
+			//인스턴스를 밖에서 맘대로 new를 하지못하게 할수 없을까?
+			//인스턴스를 생성할때마다 생성자에 접근을 해야하는데 생성자를 Private으로 두면 클래스 외부에서 접근이 불가능
+			//그렇게 클래스 내부에서 생성자를 만들어주는 메서드를 만들고 외부에서는 new를 쓰지못하게 해 한개의 인스턴스만 사용할수 있게 하는게 싱글톤 방식
+
+			//SingleTone1 c1 = new SingleTone1(); // new를 할수없음
+			SingleTone1 c1 = SingleTone1.CreateInstance(); // 이렇게 싱글톤 방식으로 인스턴스를 생성해주는 메서드를 바로 호출
+			c1.Method();
+
+			SingleTone1 c2 = SingleTone1.CreateInstance();
+			c2.Method();
+
+			c2 = SingleTone1.CreateInstance();
+			c2.Method();
+		}
+	}
+}
